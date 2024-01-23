1c1db2fad8c399bfc2202b0af16d8665d79ae418
damian.figinski <figo96it@gmail.com>
2024-01-23 20:01:45 +0100

    Create Spring Boot application

---
+++ src/main/java/damianfiginski/cms/SystemCMS/SystemCmsApplication.java
@@ -0,0 +1,13 @@
+package damianfiginski.cms.SystemCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemCmsApplication.class, args);
+	}
+
+}
